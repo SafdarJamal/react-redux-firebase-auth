fd65ca82cb83de793c83ecce33bf632493e32808
Safdar Jamal <safdar.dev@gmail.com>
2019-11-03 23:02:24 +0500

    Create header component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import Header from '../../components/Header';
 import Routes from '../../routes';
 
 const App = () => {
   return (
     <Fragment>
+      <Header />
       <Routes />
     </Fragment>
   );
