3fef0d6af1bf45bb7d253d13c1c4329b8279b3fe
Safdar Jamal <safdar.dev@gmail.com>
2019-11-04 19:03:19 +0500

    Integrate firebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Firebase, { FirebaseContext } from './services/firebase';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import store from './store';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <Router>
+        <App />
+      </Router>
+    </FirebaseContext.Provider>
   </Provider>,
   document.getElementById('root')
 );
