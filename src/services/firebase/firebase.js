3e1b44c275878e1a22e963b34554bdd2f2413fb8
Safdar Jamal <safdar.dev@gmail.com>
2020-11-17 22:30:54 +0500

    Move firebaseConfig into firebase directory

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -3,7 +3,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-import firebaseConfig from '../../config/firebaseConfig';
+import firebaseConfig from './config';
 
 class Firebase {
   constructor() {
