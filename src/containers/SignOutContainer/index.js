66b02fafdfae14dc529039de034811702e22f58e
Safdar Jamal <safdar.dev@gmail.com>
2020-11-28 18:20:53 +0500

    Create actions index

--- src/containers/SignOutContainer/index.js
+++ src/containers/SignOutContainer/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { removeUser } from '../../actions/user';
+import { removeUser } from '../../actions';
 import { withFirebase } from '../../services/firebase';
 
 import SignOut from '../../components/SignOut';
