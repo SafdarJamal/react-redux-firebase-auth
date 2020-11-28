2c4e52b12be664b64d24e663f0824855126ec70c
Safdar Jamal <safdar.dev@gmail.com>
2020-11-28 18:44:06 +0500

    Update Routes

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import * as ROUTES from '../constants/routes';
+import * as routes from '../constants/routes';
 
 const PrivateRoute = ({ user, component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      user ? <Component {...props} /> : <Redirect to={ROUTES.SIGN_IN} />
+      user ? <Component {...props} /> : <Redirect to={routes.SIGN_IN} />
     }
   />
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import * as ROUTES from '../constants/routes';
+import * as routes from '../constants/routes';
 
 import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
@@ -14,10 +14,10 @@ import NotFound from '../components/NotFound';
 const Routes = () => {
   return (
     <Switch>
-      <PublicRoute path={ROUTES.LANDING} component={Landing} exact />
-      <PublicRoute path={ROUTES.SIGN_UP} component={SignUpContainer} exact />
-      <PublicRoute path={ROUTES.SIGN_IN} component={SignInContainer} exact />
-      <PrivateRoute path={ROUTES.HOME} component={HomeContainer} exact />
+      <PublicRoute path={routes.LANDING} component={Landing} exact />
+      <PublicRoute path={routes.SIGN_UP} component={SignUpContainer} exact />
+      <PublicRoute path={routes.SIGN_IN} component={SignInContainer} exact />
+      <PrivateRoute path={routes.HOME} component={HomeContainer} exact />
       <Route component={NotFound} />
     </Switch>
   );
