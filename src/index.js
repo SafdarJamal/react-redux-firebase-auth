e8db10dd64fc5850d1268cda0d081f30bd6937ea
Safdar Jamal <safdar.dev@gmail.com>
2020-12-24 18:15:26 +0500

    Upgrade to CRA v4

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { store, persistor } from './store';
 import Firebase, { FirebaseContext } from './services/firebase';
 import './index.css';
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -21,7 +21,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
