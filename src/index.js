7a8fdbd6e442a466b812ee73fb3f7e5652095573
Safdar Jamal <safdar.dev@gmail.com>
2022-01-07 22:48:59 +0500

    Remove Web Vitals

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { store, persistor } from './store';
 import Firebase, { FirebaseContext } from './services/firebase';
 import './index.css';
 import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,8 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
